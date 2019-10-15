e67c94b899cfab9ab8df2507f8fa6f92df51a537
Diego Alejandro Reyes Espitia <diego.reyes@tars.dev>
2019-10-15 17:25:43 -0500

    Estrucutra y estilos del header

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Carousel = () => (
+  <section class="carousel">
+    <div class="carousel__container">
+        {}
+    </div>
+  </section>
+);
+
+export default Carousel;
